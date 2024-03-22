b0ec9701724e792eb9e58a5c53856dd2f1696701
Farid Maharramov <faridmaharramov@hotmail.com>
2024-03-22 13:29:09 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Simple_Docker_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 0;
+            while (i < 1000)
+            {
+                Console.WriteLine("Hello World!");
+                i++;
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
